849fcfb58cef5e4c7d8545c6a679734bebc1a151
Angela Guardia <angelaguardia20@gmail.com>
2020-07-29 08:38:48 -0700

    Add add_vendor method and tests to Market

--- test/market_test.rb
+++ test/market_test.rb
@@ -17,11 +17,11 @@ class MarketTest < Minitest::Test
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
 
-    @vendor1.stock(item1, 35)
-    @vendor1.stock(item2, 7)
-    @vendor2.stock(item4, 50)
-    @vendor2.stock(item3, 25)
-    @vendor3.stock(item1, 65)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
   end
 
   def test_it_exists
@@ -31,5 +31,13 @@ class MarketTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_a_vendor
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 end
