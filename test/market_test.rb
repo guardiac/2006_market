673600e01bcc5fba89269de91b56211100584cee
Angela Guardia <angelaguardia20@gmail.com>
2020-07-29 08:43:38 -0700

    Add vendor_names method and tests to Market

--- test/market_test.rb
+++ test/market_test.rb
@@ -40,4 +40,12 @@ class MarketTest < Minitest::Test
 
     assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
+
+  def test_it_knows_vendors_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
+  end
 end
